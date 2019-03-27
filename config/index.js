5abba58ba754dfe2f582b766e64c510fec312d61
lxw <243485908@qq.com>
2019-03-27 09:45:19 +0800

    完善功能

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: 'http://localhost:8762',  // 本地域名
+                target: 'http://192.168.20.243:8762',  // 本地域名
                 changeOrigin: true,  //是否跨域
                 pathRewrite: {
                     '^/api': '/api'   //需要rewrite重写的,
--- src/App.vue
+++ src/App.vue
@@ -5,43 +5,54 @@
 </template>
 
 <script>
-export default {
-    name: 'App',
-    data () {
-        return {
-        };
-    },
-    components: {
-    },
-    methods: {
-    }
-};
+    export default {
+        name: 'App',
+        data () {
+            return {
+            };
+        },
+        components: {
+        },
+        methods: {
+        }
+    };
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  /* 开始过渡阶段,动画出去阶段 */
-  .fade-enter-active,.fade-leave-active{
-      transition: all 0.5s ease-out;
-  }
-  /* 进入开始 */
-  .fade-enter{
-      transform: rotateY(90deg);
-      opacity: 0;
-  }
-  /* 出去终点 */
-  .fade-leave-active{
-      transform: rotateY(-90deg);
-      opacity: 0;
-  }
-  .el-input--small .el-input__inner {
-      height: 32px;
-      line-height: 46px;
-  }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    .fade-enter-active {
+        transition: opacity 2.2s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+    .el-input--small .el-input__inner {
+        height: 32px;
+        line-height: 46px;
+    }
+    .el-select {
+        width: 200px !important;
+    }
+    .el-radio-group {
+        width:200px !important;;
+    }
+    .el-input--mini .el-input__inner {
+        width:200px !important;
+    }
+    .el-date-editor.el-input, .el-date-editor.el-input__inner{
+        width:200px !important;
+    }
+    i {
+        font-size: 16px !important;
+    }
+    .el-textarea__inner{
+        width:200px !important;
+    }
+
 </style>
